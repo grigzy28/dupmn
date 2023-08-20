83bcdbbfe0f7cd19ac5eebfd05d3b3b5e9c268d2
grigzy28 <30666305+grigzy28@users.noreply.github.com>
2023-08-20 14:33:49 -0500

    Update dupmn.sh

--- dupmn.sh
+++ dupmn.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-# Author: neo3587
-# Source: https://github.com/neo3587/dupmn
+# Original Author: neo3587
+# Original Source: https://github.com/neo3587/dupmn
+# Updated Author: neo3587
+# Updated Source: https://github.com/neo3587/dupmn
 
 # TODO:
 # - AUTO_IPV6=1 => auto choose/add a IPv6 when creating on dupmn install
@@ -174,8 +176,8 @@ function configure_systemd() {
 	\nExecStop=$EXEC_COIN_CLI -conf=$COIN_FOLDER$1/$COIN_CONFIG -datadir=$COIN_FOLDER$1 stop\
 	\nRestart=always\
 	\nPrivateTmp=true\
-	\nTimeoutStopSec=60s\
-	\nTimeoutStartSec=10s\
+	\nTimeoutStopSec=6000s\
+	\nTimeoutStartSec=1000s\
 	\nStartLimitInterval=120s\
 	\nStartLimitBurst=5\
 	\n\
@@ -1021,11 +1023,11 @@ function cmd_help() {
 			\n  - ${YELLOW}dupmn swapfile <size_in_mbytes>                ${NC}Creates, changes or deletes (if parameter is ${CYAN}0${NC}) a swapfile of the given size in MB to increase the virtual memory.\
 			\n  - ${YELLOW}dupmn update                                   ${NC}Checks the last version of the script and updates it if necessary.\
 			\n**NOTE 1**: ${YELLOW}<parameter>${NC} means required, ${YELLOW}[parameter]${NC} means optional, ${YELLOW}node${NC} is always a number that refers to a dupe (${CYAN}0${NC} is the main node).\
-			\n**NOTE 2**: Check ${CYAN}https://github.com/neo3587/dupmn/wiki/Commands${NC} for extended info and usage examples of each command.\
-			\n**NOTE 3**: Check ${CYAN}https://github.com/neo3587/dupmn/wiki/FAQs${NC} for technical questions and troubleshooting."
+			\n**NOTE 2**: Check ${CYAN}https://github.com/grigzy28/dupmn/wiki/Commands${NC} for extended info and usage examples of each command.\
+			\n**NOTE 3**: Check ${CYAN}https://github.com/grigzy28/dupmn/wiki/FAQs${NC} for technical questions and troubleshooting."
 }
 function cmd_update() {
-	curl -sL https://raw.githubusercontent.com/neo3587/dupmn/master/dupmn_install.sh 3>&3 | bash
+	curl -sL https://raw.githubusercontent.com/grigzy28/dupmn/master/dupmn_install.sh 3>&3 | bash
 	exit
 }
 
